0f074cc3613e4a640b2de5386261677df3eb3aa4
gory-koxav <129182267+gory-koxav@users.noreply.github.com>
2024-03-16 22:11:37 +0900

    JS-home

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 // 페이지 아래로 스크롤 시, Header 에 다크 스타일링 적용
 const header = document.querySelector(".header");
-const headerHeight = header.getBoundingClientRect().height;
+const headerHeight = header.offsetHeight;
 
 document.addEventListener("scroll", () => {
   if (window.scrollY > headerHeight) {
@@ -8,4 +8,17 @@ document.addEventListener("scroll", () => {
   } else {
     header.classList.remove("header--dark");
   }
+});
+
+// 페이지 아래로 스크롤 시, 콘텐츠 점진적 투명화
+const home = document.querySelector(".home__container");
+const homeHeight = home.offsetHeight;
+
+document.addEventListener("scroll", () => {
+  if (window.scrollY < homeHeight) {
+    homeContainerOpacity = 1 - window.scrollY / homeHeight;
+  } else {
+    homeContainerOpacity = 0;
+  }
+  home.style.opacity = homeContainerOpacity;
 });
