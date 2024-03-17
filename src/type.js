0827aa599c089268261971a2dffebaa7016b92b1
gory-koxav <129182267+gory-koxav@users.noreply.github.com>
2024-03-17 19:46:17 +0900

    typeit

---
+++ src/type.js
@@ -0,0 +1,23 @@
+"use strict";
+
+new TypeIt(".home__title--strong", {
+  loop: true,
+  speed: 100,
+}) // Dream Coder|
+  .move(-11)
+  .type("Amazing ") // Amazing |Dream Coder
+  .pause(1000)
+  .move(null, { to: "END" }) // Amazing Dream Coder|
+  .delete() // |
+  .type("Front-end Engineer") // Front-end Engineer|
+  .pause(1000)
+  .move(-9)
+  .delete(9)
+  .type("Back-end")
+  .pause(1000)
+  .delete(8)
+  .type("Full-stack")
+  .pause(1000)
+  .move(9)
+  .delete()
+  .go();
